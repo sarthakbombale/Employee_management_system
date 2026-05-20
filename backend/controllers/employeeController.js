6271d64c044c0e9e48140c3b8f1f547e69f9f489
sarthakbombale <sarthakbomble250@gmail.com>
2026-05-20 19:37:35 +0530

    GET API

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -1,2 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const db = require('../config/db.js');
+
+
+const  getEmployees  = async (req, res) => {
+    try {
+        const [rows] = await db.query(`SELECT * FROM employee`)
+        res.status(200).json(rows)
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "server error "});
+    }
+};
+
+
+module.exports = { getEmployees };
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const mysql2 = require('mysql2');
 const db = require('./config/db.js');
 const PORT = process.env.PORT || 4000;
 
-
+const employeeRoutes = require('./routes/employeeRoutes.js');
 app.use(express.json());
 app.use(cors());
+
+
+app.use('/api/employees', employeeRoutes);
 
 app.listen(PORT, () => {
     console.log(`server is listening on Port ${PORT}`);
